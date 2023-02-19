aa21409aba9417cf68a41a33710e1403adfd7659
Alexey Rusinov <alexrus8455@gmail.com>
2023-02-19 22:13:55 +0600

    add function "while"

--- Task Sceduler/task sceduler.py
+++ Task Sceduler/task sceduler.py
@@ -7,15 +7,19 @@ show - print all tasks in list.
 # tasks list
 tasks = []
 
+run = True
 # project code for implementation
-command = input("Enter your command: ")
-if command == "help":
-    print(HELP)
-elif command == "show":
-    print(tasks)
-elif command == "add":
-    task = input("Enter name of task: ")
-    tasks.append(task)
-    print("Task add in list")
-else:
-    print("Unknown command")
+while run:
+    command = input("Enter your command: ")
+    if command == "help":
+        print(HELP)
+    elif command == "show":
+        print(tasks)
+    elif command == "add":
+        task = input("Enter name of task: ")
+        tasks.append(task)
+        print("Task add in list")
+    else:
+        print("Unknown command")
+        break
+print("Goodbye!")
