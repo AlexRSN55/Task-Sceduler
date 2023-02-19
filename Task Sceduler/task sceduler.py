1b6eb4b502b240d16585d21fca6c2fd081f7cc0b
Alexey Rusinov <alexrus8455@gmail.com>
2023-02-19 21:38:08 +0600

    init

---
+++ Task Sceduler/task sceduler.py
@@ -0,0 +1,19 @@
+HELP = """
+help - print program information.
+add - add task in list (user input name of task)
+show - print all tasks in list.
+"""
+
+tasks = []
+
+command = input("Enter your command: ")
+if command == "help":
+    print(HELP)
+elif command == "show":
+    print(tasks)
+elif command == "add":
+    task = input("Enter name of task: ")
+    tasks.append(task)
+    print("Task add in list")
+else:
+    print("Unknown command")
