1368bdc8c65cf7d58d1920085b5db243c64479d5
Alexey Rusinov <alexrus8455@gmail.com>
2023-02-21 21:16:36 +0600

    add three parameters

--- Task Sceduler/task sceduler.py
+++ Task Sceduler/task sceduler.py
@@ -1,10 +1,14 @@
+today = list()  # today = []
+tomorrow = list()  # tomorrow = []
+later = list()  # later = []
+
 # Helper
 HELP = """
 help - print program information.
 add - add task in list (user input name of task)
 show - print all tasks in list.
 """
-# tasks list
+# tasks listsceduler
 tasks = []
 
 run = True
@@ -19,6 +23,8 @@ while run:
         task = input("Enter name of task: ")
         tasks.append(task)
         print("Task add in list")
+    # elif command == "exit":
+    #     print("Спасибо за использование! До свидания!")
     else:
         print("Unknown command")
         break
