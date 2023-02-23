8458e305cc3043525bc1cff5698378567b4a35b1
Alexey Rusinov <alexrus8455@gmail.com>
2023-02-24 02:18:23 +0600

    add function "add_todo"

--- Task Sceduler/task sceduler.py
+++ Task Sceduler/task sceduler.py
@@ -7,9 +7,16 @@ random - add random task on date "today"
 """
 RANDOM_TASK = "Learning Python"
 
-tasks = {
+tasks = {}
 
-}
+def add_todo(date, task):
+    if date in tasks:
+        tasks[date].append(task)
+    # if list have not date, create date
+    else:
+        tasks[date] = []
+        tasks[date].append(task)
+    print(f"The task {task} was added on the {date} ")
 
 # project code for implementation
 while True:
@@ -19,14 +26,7 @@ while True:
     elif command == "add":
         date = input("Enter date for your task: ")
         task = input("Enter name of the task: ").capitalize()
-# if list have date, add task in list
-        if date in tasks:
-            tasks[date].append(task)
-# if list have not date, create date
-        else:
-            tasks[date] = []
-            tasks[date].append(task)
-        print(f"The task {task} was added on the {date} ")
+        add_todo(date, task)
 
     elif command == "show":
         date = input("Enter date for show tasks list ")
@@ -37,11 +37,7 @@ while True:
             print('No such date')
 
     elif command == "random":
-        if "today" in tasks:
-            tasks["today"].append(RANDOM_TASK)
-        else:
-            tasks["today"] = []
-            tasks["today"].append(RANDOM_TASK)
+        add_todo('today', RANDOM_TASK)
     else:
         print("Unknown command")
         break
