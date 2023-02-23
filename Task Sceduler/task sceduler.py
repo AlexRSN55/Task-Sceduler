804d3f700c7dadfa121a51782d5081919ebdd2bf
Alexey Rusinov <alexrus8455@gmail.com>
2023-02-24 01:43:04 +0600

    create enumeration of task

--- Task Sceduler/task sceduler.py
+++ Task Sceduler/task sceduler.py
@@ -11,23 +11,25 @@ while True:
     command = input("Enter your command: ")
     if command == "help":
         print(HELP)
-
     elif command == "add":
         date = input("Enter date for your task: ")
-        task = input("Enter name of the task: ")
-
-        # if list have date, add task in list
+        task = input("Enter name of the task: ").capitalize()
+# if list have date, add task in list
         if date in tasks:
             tasks[date].append(task)
-
-        # if list have not date, create date
+# if list have not date, create date
         else:
             tasks[date] = []
             tasks[date].append(task)
         print(f"The task {task} was added on the {date} ")
 
     elif command == "show":
-        print(tasks)
+        date = input("Enter date for show tasks list ")
+        if date in tasks:
+            for task in tasks[date]:
+                print('-', task)
+        else:
+            print('No such date')
 
     else:
         print("Unknown command")
